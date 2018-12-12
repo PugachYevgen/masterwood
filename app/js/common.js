35910e1c34c7ce3e2f99409ffc03bb53627f1cfc
USER-PC\Tom <jedi.wasya@gmail.com>
2018-12-12 21:38:44 +0200

    initial commit

---
+++ app/js/common.js
@@ -0,0 +1,63 @@
+$(function() {
+
+    /********* Карусель *******/
+
+    var slideNow = 1;
+    var slideCount = $('#slidewrapper').children().length; /* возвращает кол-во таблиц в слайдере */
+    var translateWidth = 0;
+    $(document).ready(function() {
+
+
+        $('#next-btn').click(function() {
+            nextSlide();
+            return false
+        });
+
+        $('#prev-btn').click(function() {
+            prevSlide();
+            return false
+        });
+
+
+    });
+
+
+    function nextSlide() {
+        if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+            $('#slidewrapper').css('transform', 'translate(0, 0)');
+            slideNow = 1;
+        } else {
+            translateWidth = -$('#viewport').width() * (slideNow);
+            $('#slidewrapper').css({
+                'transform': 'translate(' + translateWidth + 'px, 0)',
+                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            });
+            slideNow++;
+        }
+    }
+
+    function prevSlide() {
+        if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+            translateWidth = -$('#viewport').width() * (slideCount - 1);
+            $('#slidewrapper').css({
+                'transform': 'translate(' + translateWidth + 'px, 0)',
+                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            });
+            slideNow = slideCount;
+        } else {
+            translateWidth = -$('#viewport').width() * (slideNow - 2);
+            $('#slidewrapper').css({
+                'transform': 'translate(' + translateWidth + 'px, 0)',
+                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            });
+            slideNow--;
+        }
+    }
+
+});
+
+
+
